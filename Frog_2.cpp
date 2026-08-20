7be0899f35c04ccc664c56e0854952bb196b08aa
Tanjim100 <tanjim.haider.12112100@gmail.com>
2026-08-20 23:38:47 +0600

    #2

---
+++ Frog_2.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define cinvec(v,n) for(int i=0;i<n;i++){ll x; cin>>x; v.push_back(x);}
+#define coutvec(v)  for(auto x : v){cout << x << " ";}cout << endl
+#define maxof(v) *max_element(v.begin(), v.end())
+#define minof(v) *min_element(v.begin(), v.end())
+#define sort(v)   sort(v.begin(), v.end()) 
+
+const ll mod = 1e9+7;
+
+
+ll gcd(ll a, ll b)
+{
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
+ll nCr(int n, int r)
+{
+    ll p = 1, k = 1;
+
+    if(n - r < r)
+    {
+        r = n - r;
+    }
+
+    if(r)
+    {
+        while(r)
+        {
+            p *= n;
+            k *= r;
+
+            ll m = gcd(p, k);
+
+            p /= m;
+            k /= m;
+
+            n--;
+            r--;
+        }
+    }
+    else p = 1;
+
+    return p;
+}
+
+
+bool isPrime(ll n)
+{
+    bool flag = true;
+    for (int i = 2; i*i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            flag = false;
+            return flag;
+        } 
+    }
+    return flag;
+}
+
+
+ll bigmod(ll a, ll b, ll M)
+{
+    if(b == 0) return 1 % M;
+    ll x = bigmod(a, b/2, M);
+    x = (x * x) % M;
+
+    if(b & 1) x = (x * a) % M;
+
+    return x;
+}
+
+void make_bitmask(vector< vector<int> > &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        int q = i;
+        int j = 0;
+        while(q)
+        {
+            v[i][j] = q % 2;
+            q /= 2;
+            j++;
+        }
+    }
+    
+}
+
+
+void fast()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+ll dp[100005];
+
+ll n, k;
+
+ll func(ll ind, vector<ll> &v)
+{
+    if(ind == n-1) return 0;
+
+    if(dp[ind] != -1) return dp[ind];
+
+    ll ans = LLONG_MAX;
+
+    for (int i = 1; i <= k; i++)
+    {
+        if(ind + i < n) ans = min(ans, func(ind + i, v) + abs(v[ind] - v[ind+i]));
+    }
+    
+
+    return dp[ind] = ans;
+    
+}
+
+void solve()
+{
+    cin >> n >> k;
+    vector<ll> v;
+    cinvec(v, n);
+
+    for(ll i = 0; i <= n; i++)
+    {
+        dp[i] = -1;
+    }
+
+    ll ans = func(0, v);
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast();
+
+    int t=1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
